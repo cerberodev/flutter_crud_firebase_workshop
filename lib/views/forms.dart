c9f5a2e3aebeae52b0d34517fad45ce250a6ac62
Pierre Guillen <hi@cerbero.dev>
2020-07-15 20:45:02 -0500

    Convirtiendo Forms en StatefulWidget

--- lib/views/forms.dart
+++ lib/views/forms.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-class Forms extends StatelessWidget {
+class Forms extends StatefulWidget {
   const Forms({Key key}) : super(key: key);
 
   @override
+  _FormsState createState() => _FormsState();
+}
+
+class _FormsState extends State<Forms> {
+  @override
   Widget build(BuildContext context) {
     return Form(
+      key: _validateForm,
       child: Column(
         children: [TextFormField()],
       ),
