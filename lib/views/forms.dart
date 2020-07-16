d9da6d281a2c70164f267f8d94503c07d08d3f5b
Pierre Guillen <hi@cerbero.dev>
2020-07-15 21:49:46 -0500

    Agregando todos los TextFormField

--- lib/views/forms.dart
+++ lib/views/forms.dart
@@ -11,19 +11,53 @@ class Forms extends StatefulWidget {
 class _FormsState extends State<Forms> {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      //key: _validateForm,
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: 'Name', suffixIcon: Icon(Icons.assignment_ind)),
-          ),
-          TextFormField(
-            decoration: InputDecoration(suffixIcon: Icon(Icons.account_circle)),
-          ),
-          TextFormField()
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Form(
+        //key: _validateForm,
+        child: Column(
+          children: [
+            TextFormField(
+              autofocus: false,
+              onChanged: (null),
+              decoration: InputDecoration(
+                  labelText: 'Nombre',
+                  hintText: 'Nombre',
+                  suffixIcon: Icon(Icons.assignment_ind),
+                  //border: Border(),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Color(0xFF1B5E20),
+                    width: 2,
+                  ))),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Apellido',
+                  suffixIcon: Icon(Icons.account_circle)),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'DNI',
+                  suffixIcon: Icon(Icons.)),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Calificacion I',
+                  suffixIcon: Icon(Icons.filter_1),
+            )),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Calificacion II',
+                  suffixIcon: Icon(Icons.filter_2),
+            )),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Calificacion III',
+                  suffixIcon: Icon(Icons.filter_3),
+            )),
+          ],
+        ),
       ),
     );
   }
