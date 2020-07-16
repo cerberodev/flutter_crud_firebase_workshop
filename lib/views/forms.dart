e3f89cac9809d7017c8cd37a63a0e803117ac421
Pierre Guillen <hi@cerbero.dev>
2020-07-15 21:02:02 -0500

    Agregando labelText y suffixIcon a los TextFormField

--- lib/views/forms.dart
+++ lib/views/forms.dart
@@ -5,15 +5,25 @@ class Forms extends StatefulWidget {
 
   @override
   _FormsState createState() => _FormsState();
+  //final _validateForm;
 }
 
 class _FormsState extends State<Forms> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _validateForm,
+      //key: _validateForm,
       child: Column(
-        children: [TextFormField()],
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: 'Name', suffixIcon: Icon(Icons.assignment_ind)),
+          ),
+          TextFormField(
+            decoration: InputDecoration(suffixIcon: Icon(Icons.account_circle)),
+          ),
+          TextFormField()
+        ],
       ),
     );
   }
